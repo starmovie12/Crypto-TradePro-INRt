8a6192ff915d01a52310fc943e1099a8c964bc3f
starmovie12 <64631955+starmovie12@users.noreply.github.com>
2026-09-03 19:53:16 +0530

    Export additional types from index.ts

--- lib/api-zod/src/generated/types/index.ts
+++ lib/api-zod/src/generated/types/index.ts
@@ -6,6 +6,8 @@
  * OpenAPI spec version: 0.1.0
  */
 
+export * from './addFundsInput';
+export * from './addFundsResult';
 export * from './advisorRecommendation';
 export * from './advisorRecommendationDirection';
 export * from './getOptionChainParams';
